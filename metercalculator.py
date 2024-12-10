6f9ee89abf2f4447a49f7f674d03e3e90e92372c
Jothi <jothi/90mani@gmail.com>
2024-12-10 09:35:50 -0800

    implemented exit button

--- metercalculator.py
+++ metercalculator.py
@@ -1,6 +1,8 @@
 import FreeSimpleGUI as sg
 from convertor import feet_inch_calc
 
+sg.theme('Black')
+
 label1=sg.Text("Enter the Feet")
 input1=sg.InputText(key='Feet')
 
@@ -8,16 +10,26 @@ label2=sg.Text("Enter the Inches")
 input2=sg.InputText(key='Inches')
 
 convert_button = sg.Button("Convert")
+exit_button=sg.Button("Exit")
 output_button=sg.Text(key='output')
 window = sg.Window('Calculator',layout=[[label1,input1],
                                         [label2,input2],
-                                        [convert_button,output_button]])
+                                        [convert_button,exit_button,output_button]])
 
 while True:
     event,values=window.read()
-    feet=float(values['Feet'])
-    inches=float(values['Inches'])
-    meter=feet_inch_calc(feet, inches)
-    print(meter)
-    window['output'].update(value=f"{meter}m.Converted successfully")
+    try:
+        feet=float(values['Feet'])
+        inches=float(values['Inches'])
+        meter=feet_inch_calc(feet, inches)
+        print(meter)
+        window['output'].update(value=f"{meter}m.Converted successfully")
+    except ValueError:
+        sg.popup("Please enter two numbers")
+
+    match event:
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
 window.close()
